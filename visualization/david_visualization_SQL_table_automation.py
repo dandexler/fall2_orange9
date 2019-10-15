0879ae8288f08990ad18f5be78de7dfe24ec392b
dandexler <dandexler@rzr.localdomain>
2019-10-15 00:02:29 -0400

    Added better process, comments, general tidying

--- visualization/david_visualization_SQL_table_automation.py
+++ visualization/david_visualization_SQL_table_automation.py
@@ -1,68 +1,94 @@
+##############################################################
+#
+# David Andexler
+# Institute for Advanced Analytics - NCSU
+# Visualization Project - Automated SQL Database Generator
+#
+##############################################################
+
+import os
+import pandas as pd
 import sqlite3 as sql
 from sqlite3 import Error
-import csv
 from tkinter import filedialog
 from tkinter import *
+
+
+# Create list of year range for data
+# Adjust based on data needs
+# This will be used to create the table names
+years = [x for x in range(2000, 2018)]
 
 
 def main():
     # Explicitly sets Connection object to null
     Connection = None
 
-    # Location of local DB
+    # Local database selection prompt
+    # Select the database that is getting new tables
     try:
         root = Tk()
         root.withdraw()
-        db_file = filedialog.askopenfilename(title="Select database: ")
-        print(db_file)
+        db_file = filedialog.askopenfilename(title='Select database: ')
     except:
-        print("FileImport error.")
+        print('DBImport error.')
         quit()
 
     # Establish connection with DB
     try:
         Connection = sql.connect(db_file)
-        print("Connection initialized.")
     except Error as e:
-        print("Invalid connection.")
-        print("Error reference:", e)
-
-
-
-    # Reads in CSV file, reads first line to extract column names
-    try:
-        first_file = filedialog.askopenfilename(title="Select file to read "
-                                                  "columns:")
-    except:
-        print("Error.")
-        quit()
-
-    with open(first_file, 'r') as file:
-        reader = csv.reader(file)
-        column_names = next(reader)
-        column_names = ','.join(map(str, column_names))
-
-    # Assumes that we want to create tables for 2000-2017 inclusive. Must
-    # change below line if subsetting data
-    years = [x for x in range(2000, 2018)]
-    print("Columns read in:", column_names)
-    print("Years:", years)
-
-    # Create table for each year, reading in column names
+        print('Invalid connection.')
+        print('Error reference:', e)
+    print('Connection initialized.')
+    # Connection object cursor
     c = Connection.cursor()
 
-    # Creates Tables for each year using the column names extracted previously
+    # Data directory prompt
+    try:
+        directory = filedialog.askdirectory(title='Select directory with ALL '
+                                                  'data:') + '/'
+    except:
+        print('Unknown error.')
+        quit()
 
-    # Delete table if name exists, uncomment and use this code if for some
-    # reason you run this multiple times
-    #for year in years:
-    #   c.execute("DROP TABLE av_{}".format(year))
+    # Gets names of all files in the selected data directory
+    data = [directory + filename for filename in os.listdir(directory) if filename.endswith('.csv')]
+    print("CSV filenames read in:", data)
 
-    # Creates table for each year
-    for year in years:
-        c.execute("CREATE TABLE av_{} ({})".format(year, column_names))
+    # Variable selection. Group-agreed method: Intersection of all tables.
+    # All SQL tables will be initialized using common_variables,
+    # the variables that all tables have.
+    variables = []
+    for i in range(len(data)):
+        print('Reading in av_{}'.format(years[i]))
+        df = pd.read_csv(data[i], encoding='latin')
+        variables.append(set(df.columns))
+    for i in range(len(variables)):
+        print('\n')
+        print('Year', years[i])
+        print('Variable Count:', len(variables[i]))
+        print(variables[i])
+    print('Intersection of all variables:')
+    common_variables = set.intersection(*variables)
+    print('Length of intersection:', len(common_variables))
+    print(common_variables)
+    common_variables = list(common_variables)
+
+    # Extracts column names from common_variables
+    print('Columns read in:', common_variables)
+
+    # Reads in each CSV data file, converts to pandas data frame, then reads
+    # it to SQL database
+    for i in range(len(years)):
+        df = pd.read_csv(data[i], encoding='latin')
+        df = df[common_variables]
+        print('Reading file:', data[i])
+        df.to_sql(name='av_{}'.format(years[i]), con=Connection,
+                  if_exists='replace')
 
     # Close connection with DB
+    print('Process complete.')
     Connection.close()
 
 main()
