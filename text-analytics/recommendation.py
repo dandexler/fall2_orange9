83a8ed3030e07f534ef47ac05d503fb50592013b
dandexler <dandexler@gmail.com>
2019-10-21 01:14:56 -0400

    Command line functionality and general housekeeping

--- text-analytics/recommendation.py
+++ text-analytics/recommendation.py
@@ -1,6 +1,12 @@
 import pickle
 import pandas as pd
+import os
 
+# __file__ will throw error if running from interpreter. Run from bash/cmd.exe
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+# Full filepath to pickle data. Assumed to be in same directory as recommendation.py
+myfile_path = os.path.join(ROOT_PATH, "store.pckl")
 
 # Load all data from pickle data dump. Data assumed to be in same working directory as recommendation.py
 def loadall(filename):
@@ -12,10 +18,14 @@ def loadall(filename):
                 break
 
 
+# Call to function loadall, which loads pickle data
+items = list(loadall("store.pckl"))
+
+
 # Recommendation generator with data pulled in from pickle file
 def recommendations(title, cosine_sim=items[0]):
     # Empty list of recommended movies
-    recommended_movies = []
+    recommended_talks = []
 
     # Extracts index of movie that matches title
     idx = indices[indices == title].index[0]
@@ -28,15 +38,22 @@ def recommendations(title, cosine_sim=items[0]):
 
     # Appends top 10 movies recommended_movies
     for i in top_10_indexes:
-        recommended_movies.append(list(items[1].index)[i])
+        recommended_talks.append(list(items[1].index)[i])
 
     # Returns final list
-    print("Recommended for you, Orange Team 9:")
-    return recommended_movies
-
-
-# Call to function loadall, which loads pickle data
-items = list(loadall("store.pckl"))
+    return recommended_talks
 
 # List index of title names
 indices = pd.Series(items[1].index)
+
+# Intended to run from the command line, allows user to continually input title of talk
+while True:
+    inp = input("Type a TED Talk title name or q to quit: ")
+    if inp.lower() == 'q':
+        print("Exit.")
+        quit()
+    else:
+        print("\nRecommended for you, Orange Team 9:")
+        rec_talks = recommendations(inp)
+        for i in rec_talks:
+            print(i)
